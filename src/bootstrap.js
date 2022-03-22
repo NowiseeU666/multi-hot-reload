e9991764dbcd2b99cb2c4065a9fcbaec236866bf
Nowisee <7383077+nowisee@user.noreply.gitee.com>
2022-03-22 16:17:54 +0800

    chore: rename multi-hot-reload

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -7,9 +7,9 @@ const hotServerInstance = require("./server");
 let config = "";
 
 try{
-  config = require(path.resolve(cwdPath, "./.template-hot-server.json"));
+  config = require(path.resolve(cwdPath, "./multi-hot-reload.json"));
 } catch {
-  throw new Error("Cannot find file .template-hot-server.json");
+  throw new Error("Cannot find file multi-hot-reload.json");
 }
 
 hotServerInstance.start(config);
